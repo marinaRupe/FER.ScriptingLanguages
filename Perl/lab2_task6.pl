f75775a93f339ba8bf17cf951c5602a2d1157333
Marina Rupe <marina.rupe@yahoo.com>
2016-05-22 14:26:22 +0200

    Added the Perl script for the sixth task.

---
+++ Perl/lab2_task6.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+$first_letter = uc(shift(@ARGV));
+$shift = (ord($first_letter) - ord('A')) % 26;
+
+while(<>) {
+	$line = uc($_);
+	foreach $c (split //, $line) {
+		if (ord($c) >= ord('A') and ord($c) <= ord('Z')) {
+			$distance = ord($c) - $shift; 
+			$backwards = ord('A') - $distance;
+			
+			if ($backwards <= 0) {
+                $char = chr(ord($c) - $shift);
+            }
+			else {
+				$char = chr(ord('Z') - $backwards + 1);
+			}
+        }
+		else {
+			$char = $c;
+		}
+        print "$char";
+	}
+	
+	if (eof) {
+        print "\n";
+    }
+}

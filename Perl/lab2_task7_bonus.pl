49bae6553eb9dda036d116ce47afcae0f8e8ad1c
Marina Rupe <marina.rupe@yahoo.com>
2016-05-22 14:31:26 +0200

    Added the Perl script for the seventh task.

---
+++ Perl/lab2_task7_bonus.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+#Bonus task: for every client in the file calculate and print the number of server accesses. Sort the print descending.
+#Argument: localhost_access_log.txt
+
+while (<>) {
+	$line = $_;
+    @data = split / /, $line;
+    $ip = $data[0];
+    $clients{$ip} +=1;
+}
+
+@number_of_accesses = (reverse sort {$a <=> $b} values %clients);
+$n = @number_of_accesses;
+
+$i = 0;
+while ($i < $n) {
+    foreach $ip (keys %clients) {
+        if ($clients{$ip} == $number_of_accesses[$i]) {
+            print "$ip : $number_of_accesses[$i]\n";
+            $i += 1;
+            delete $clients{$ip};
+        }
+    }
+}
+    
+ 
